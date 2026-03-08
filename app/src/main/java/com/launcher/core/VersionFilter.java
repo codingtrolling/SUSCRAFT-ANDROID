eb0f6c5e4523e68fa5c0ad76e0d6fc03af25f670
codingtrolling <rabi65171@gmail.com>
2026-03-08 22:06:27 +0000

    Fix: Add JSONException handling in VersionFilter

--- app/src/main/java/com/launcher/core/VersionFilter.java
+++ app/src/main/java/com/launcher/core/VersionFilter.java
@@ -1,26 +1,43 @@
 package com.launcher.core;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import android.util.Log;
 import java.util.ArrayList;
 import java.util.List;
 
 public class VersionFilter {
-    public static List<VersionInfo> filterReleases(String manifestJson) {
-        List<VersionInfo> releases = new ArrayList<>();
-        JSONObject root = new JSONObject(manifestJson);
-        JSONArray versions = root.getJSONArray("versions");
+    private static final String TAG = "VersionFilter";
 
-        for (int i = 0; i < versions.length(); i++) {
-            JSONObject v = versions.getJSONObject(i);
-            if (v.getString("type").equals("release")) {
-                releases.add(new VersionInfo(
-                    v.getString("id"),
-                    v.getString("type"),
-                    v.getString("url")
-                ));
+    public List<VersionInfo> filterReleases(String manifestJson) {
+        List<VersionInfo> releases = new ArrayList<>();
+        try {
+            JSONObject root = new JSONObject(manifestJson);
+            JSONArray versions = root.getJSONArray("versions");
+
+            for (int i = 0; i < versions.length(); i++) {
+                JSONObject v = versions.getJSONObject(i);
+                if ("release".equals(v.getString("type"))) {
+                    releases.add(new VersionInfo(
+                        v.getString("id"),
+                        v.getString("type"),
+                        v.getString("url")
+                    ));
+                }
             }
+        } catch (JSONException e) {
+            Log.e(TAG, "Error filtering versions from manifest", e);
         }
         return releases;
+    }
+
+    public static class VersionInfo {
+        public String id, type, url;
+        public VersionInfo(String id, String type, String url) {
+            this.id = id;
+            this.type = type;
+            this.url = url;
+        }
     }
 }
