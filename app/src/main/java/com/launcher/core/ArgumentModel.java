ad55b5aa75f3fa994db61b98bf24a1f2e30b4424
codingtrolling <rabi65171@gmail.com>
2026-03-08 21:56:38 +0000

    Fix: Handle JSONException in ArgumentModel

--- app/src/main/java/com/launcher/core/ArgumentModel.java
+++ app/src/main/java/com/launcher/core/ArgumentModel.java
@@ -1,23 +1,30 @@
 package com.launcher.core;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import android.util.Log;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ArgumentModel {
-    public static List<String> parseGameArgs(String json) {
-        List<String> list = new ArrayList<>();
-        JSONObject obj = new JSONObject(json);
-        if (obj.has("arguments")) {
-            JSONArray gameArgs = obj.getJSONObject("arguments").getJSONArray("game");
-            for (int i = 0; i < gameArgs.length(); i++) {
-                Object arg = gameArgs.get(i);
-                if (arg instanceof String) {
-                    list.add((String) arg);
+    private static final String TAG = "ArgumentModel";
+    public final List<String> gameArguments = new ArrayList<>();
+
+    public void parseArgs(String json) {
+        try {
+            JSONObject obj = new JSONObject(json);
+            if (obj.has("arguments") && obj.getJSONObject("arguments").has("game")) {
+                JSONArray gameArgs = obj.getJSONObject("arguments").getJSONArray("game");
+                for (int i = 0; i < gameArgs.length(); i++) {
+                    Object arg = gameArgs.get(i);
+                    if (arg instanceof String) {
+                        gameArguments.add((String) arg);
+                    }
                 }
             }
+        } catch (JSONException e) {
+            Log.e(TAG, "Error parsing game arguments", e);
         }
-        return list;
     }
 }
